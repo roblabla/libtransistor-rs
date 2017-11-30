52f8bb41b7488146d738b9dcf05d0ad91ebd6e50
roblabla <unfiltered@roblab.la>
2017-11-30 12:33:45 +0100

    Some changes required for stdlib integration

--- libtransistor-sys/src/lib.rs
+++ libtransistor-sys/src/lib.rs
@@ -1,5 +1,10 @@
+#![crate_type = "rlib"]
 #![no_std]
+
+#[cfg(not(feature = "libc"))]
 extern crate cty;
+#[cfg(feature = "libc")]
+extern crate libc as cty;
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 //include!("bindings.rs");
