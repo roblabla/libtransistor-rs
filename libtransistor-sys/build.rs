a88e46b5f853e991c1aa8342b4f081d74a047590
roblabla <unfiltered@roblab.la>
2018-01-30 16:07:22 +0100

    update libtransistor to latest

--- libtransistor-sys/build.rs
+++ libtransistor-sys/build.rs
@@ -6,32 +6,24 @@ use std::fs::File;
 use std::path::PathBuf;
 
 fn main() {
-    let dir = if cfg!(feature = "libc") {
-        // stub build !
-        env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR must be set") + "/../libtransistor-rs/libtransistor-sys"
-    } else {
-        env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR must be set")
-    };
+    let dir = env::var("LIBTRANSISTOR_HOME").expect("LIBTRANSISTOR_HOME musts be set");
 
     let status = Command::new("make")
-        .current_dir(&format!("{}/libtransistor", dir))
+        .current_dir(&format!("{}", dir))
         .status().expect("Make failed");
     if !status.success() {
         panic!("Make failed");
     }
 
-    println!("cargo:rustc-link-lib=static=transistor.nro");
     // Don't bother linking libc, liblibc takes care of it.
     // TODO: What if liblibc isn't linked ?
     // Solution: have libtransistor-sys use libc always, instead of cty.
     //println!("cargo:rustc-link-lib=static=c");
-    // TODO: It'd be nice if I didn't have to worry about those two.
-    println!("cargo:rustc-link-lib=static=ssp");
-    println!("cargo:rustc-link-lib=static=clang_rt.builtins-aarch64");
-    println!("cargo:rustc-link-search=native={}/libtransistor/build/lib", dir);
-    println!("cargo:rustc-link-search=native={}/libtransistor/build/compiler-rt/lib/linux", dir);
-    println!("cargo:rustc-link-search=native={}/libtransistor/libssp", dir);
-    println!("cargo:rustc-link-search=native={}/libtransistor/newlib/aarch64-none-switch/newlib", dir);
+    // TODO: compiler_builtins takes care of this !
+    //println!("cargo:rustc-link-lib=static=clang_rt.builtins-aarch64");
+    //println!("cargo:rustc-link-search=native={}/libtransistor/build/compiler-rt/lib/linux", dir);
+    println!("cargo:rustc-link-lib=static=transistor.nro");
+    println!("cargo:rustc-link-search=native={}/build/lib", dir);
 
     let out_path = PathBuf::from(env::var("OUT_DIR").expect("OUT_DIR must be set"));
 
@@ -56,14 +48,15 @@ fn main() {
 
     // TODO: Avoid generating a ton of useless cruft from liblibc
     let bindings = bindgen::Builder::default()
-        .header(format!("{}/libtransistor/include/libtransistor/nx.h", dir))
+        .header(format!("{}/include/libtransistor/nx.h", dir))
         // Don't use host headers, to make sure we're building against newlib
-        .clang_arg("-nostdinc")
+        .clang_arg("-nostdlibinc")
         // Include the newlib/transistor headers, and the clang builtin headers
         .clang_args(&["-isystem", "/usr/lib/clang/5.0.0/include"])
-        .clang_args(&["-isystem", &format!("{}/libtransistor/newlib/newlib/libc/include", dir)])
-        .clang_args(&["-isystem", &format!("{}/libtransistor/newlib/newlib/libc/sys/switch/include", dir)])
-        .clang_arg(format!("-I{}/libtransistor/include", dir))
+        .clang_args(&["-isystem", &format!("{}/newlib/newlib/libc/sys/switch/include", dir)])
+        .clang_args(&["-isystem", &format!("{}/build/newlib/aarch64-none-switch/newlib/targ-include", dir)])
+        .clang_args(&["-isystem", &format!("{}/newlib/newlib/libc/include", dir)])
+        .clang_arg(format!("-I{}/include", dir))
         // We don't need to define those types, rust has them already anyways.
         // Blacklisting avoids a bug in bindgen where it creates cyclic references
         // (pub type u8 = u8)
@@ -73,7 +66,7 @@ fn main() {
         .use_core()
         .ctypes_prefix("cty")
         .rustfmt_bindings(true)
-        .rustfmt_configuration_file(None)
+        .generate_comments(true)
         .generate()
         .expect("Unable to generate bindings");
 
